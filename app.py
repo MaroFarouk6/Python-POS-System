0af541da0b0ed369d427df7d65df1ea51403d164
MaroFarouk6 <113977213+MaroFarouk6@users.noreply.github.com>
2025-12-21 10:58:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1443,10 +1443,11 @@ permited_id = cr.fetchone()[0]
 if sha256(device_id.encode()).hexdigest() != permited_id:
     messagebox.showerror("Error", "This device is not authorized to run this application.")
     exit()
-elif not permited_id:
+elif permited_id:
     cr.execute(f"INSERT INTO info (device_id) VALUES {device_id}")
     db.commit()
     root.mainloop()
 else:
     root.mainloop()
+
 
